0186ae9910bd37d114f396017e4841b7e206e4ff
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-09-14 16:31:26 -0300

    style: configure prettierc file

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  endOfLine: 'lf',
+};
+
+export default config;
